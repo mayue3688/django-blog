fba1982a1bce46519afa872431865355d7022369
Yue <you@example.com>
2021-09-11 17:48:19 -0700

    lesson 07

--- blogging/migrations/0001_initial.py
+++ blogging/migrations/0001_initial.py
@@ -6,9 +6,8 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     initial = True
-
+    atomic = False
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
