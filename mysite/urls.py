6c99d6b49ba6ce4a2364caeed02a101888598f1d
Yue <you@example.com>
2021-09-13 20:31:29 -0700

    Updated

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
 
+
 urlpatterns = [
     path('', include('blogging.urls')),
     path('polling/', include('polling.urls')),
